c68837a83ae0b8ad4fa2c5a4130d542da8767b9c
wellnemo <70764962+wellnemo@users.noreply.github.com>
2020-09-07 10:05:54 +0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,344 @@
+<?php
+$useragent=$_SERVER['HTTP_USER_AGENT'];
+if(preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i',$useragent)||preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',substr($useragent,0,4))){
+?>
+
+
+<style>
+body{background:#1aa0f7;color:#fff;margin:0;padding:0;}
+.active{border:4px #000 solid;padding: 8px 5px 10px 5px;}
+.wbutton{color: #000;
+    background: #fff;
+    border-radius: 15px;
+    padding: 5px;
+    font-size: 11px;text-decoration: none;}
+</style>
+
+
+
+<div style="background:#fff;height:50px;color:#000;margin-top: 20px;">
+
+<a href="#"><img src="login.jpg" style="float: right;padding:10px 15px 10px 5px;"/></a>
+<a href="#"><img src="sett.jpg" style="float: right;padding: 10px 5px;"/></a>
+
+<div style="float:right;font-size:25px;padding:10px;"><a style="text-decoration: none;color: #000;font-size:13px" href="#">Авторынок<br/>Лениногорск</a></div>
+<a href="#"><img src="lg.jpg" style="float: right;width:50px;padding: 5px 0 0 0;"/></a>
+
+
+</div>
+
+<div style="background:#3377a4;height:80px;padding:10px 0 0px 0;color:#fff;">
+<a class="active" href="#" style="font-size: 13px;color: #fff;text-decoration: none;margin:  10px 10px 10px 0;">Покупка трафика</a> <a href="#" style="font-size: 13px;color: #fff;text-decoration: none;margin: 10px;">Продажа трафика</a> <a href="#" style="font-size: 13px;color: #fff;text-decoration: none;margin: 10px;">Агентам</a>
+
+<div style="clear:both"></div>
+<div style="margin-top:25px">
+<div style="  padding: 5px 10px;  float: right;
+"><a style="" href="#" class="wbutton">Пополнить счет</a></div>
+<div style="   padding: 5px 10px; 
+ float: right;
+    "><a style="" href="#" class="wbutton">История</a> </div>
+<div style="margin-right: 10px;float:right;color:#fff">
+<p style="padding: 0px;
+    margin: 0px;
+    font-size: 12px;">На счете:</p> 
+<p style="padding: 0px;
+    margin: 0px;
+    font-size: 12px;">0.00 р</p>
+ </div>
+</div>
+<div style="clear:both"></div>
+<div style="font-size: 30px;margin: 40px 0 0 20px;font-weight: bold;">
+Рекламные компании
+</div>
+<div style="clear:both"></div>
+<div style="margin-bottom: 20px;">
+<div style="float:left;margin-right: 10px;">
+<div style="margin: 10px 0 0 35px;
+    font-size: 20px;
+    font-weight: bold;">Ограничение аккаунта</div>
+<div style="clear:both"></div>
+<div style="background:#94caed;float:left;    margin: 15px 5px 0 35px;
+    padding: 10px;width:70px;    height: 200px;
+">
+<div style="font-weight: bold;font-size: 13px;">
+
+<div>Переходы <img width="12" style="float:right" src="down1.jpg"/>
+</div>
+</div>
+
+<div style="font-size: 12px;
+    margin-top: 12px;">
+<div style="margin-bottom: 10px;">Максимум аккаунта <br/><span style="color:#000">не ограничен</span></div>
+<div style="margin-bottom: 10px;">Получено: <span style="color:#000">0</span></div>
+<div style="margin-bottom: 10px;">Суточный лимит</div>
+<div>Будни: <span style="color:#000">макс.</span></div>
+<div>Выходные: <span style="color:#000">макс.</span></div>
+</div>
+
+</div>
+<div style="background:#94caed;float:left;margin: 15px 5px 0 0px;
+    padding: 10px;width:70px;    height: 200px;">
+	<div style="font-weight: bold;font-size: 13px;">Бюджет</div>
+	<div style="font-size: 12px;
+    margin-top: 12px;">
+	<div style="margin-bottom: 10px;">Общий на аккаунт: <br/><span style="color:#000">не ограничен</span></div>
+	<div style="margin-bottom: 10px;">Потрачено: <span style="color:#000">0</span></div>
+	<div style="margin-bottom: 10px;">Суточный лимит</div>
+	<div>Будни: <br/><span style="color:#000">не ограничен</span></div>
+	<div>Выходные: <br/><span style="color:#000">не ограничен</span></div>	
+	</div>
+	</div>
+<div style="background:#94caed;float:left;margin: 15px 5px 0 0px;
+    padding: 10px;width:70px;    height: 200px;">
+	<div style="font-weight: bold;font-size: 13px;">Расписание</div>
+	<div style="font-size: 12px;
+    margin-top: 12px;">
+	<div style="margin-bottom: 10px;">Период трансляции: <br/><span style="color:#000">не ограничен</span></div>
+	<div style="margin-bottom: 10px;">Время: <br/><span style="color:#000">не ограничен</span></div>
+	</div>
+	
+	</div>
+</div>
+<div style="clear:both"></div>
+<div style="float:left;margin-left:30px">
+<div  style="font-size: 20px;
+    font-weight: bold;margin-top:10px">Настройки по умолчанию</div>
+<div style="clear:both"></div>
+<div style="background:#94caed;margin: 15px 5px 0 0px;
+    padding: 10px;    height: 200px;
+">
+<div style="font-weight: bold;font-size: 13px;">
+Таргетинг</div>
+	<div style="font-size: 12px;
+    margin-top: 12px;">
+	<div>Регион: <span style="color:#000">не ограничен</span></div>
+<div>Тематика объявления: <span style="color:#000">легковые автомобили</span></div>
+</div>
+</div>
+</div>
+<div style="clear:both"></div>
+</div>
+<div style="clear:both"></div>
+
+
+<div style="margin:0 20px">
+
+<div>
+<div style="margin-bottom: 15px;float:left">
+<a style="" href="#" class="wbutton">Создать компанию</a> или <a style="" href="#" class="wbutton">Добавить объявление</a>
+</div>
+<div style="clear:both"></div>
+<div style="float:right;font-size:12px">
+<div style="float:right;">Статистика за период: <br/><a style="color:#fff" href="#"><img src="calendar.jpg"/> Сегодня<img src="down.jpg"/></a></div>
+<div style="float:right;margin-right: 15px;">Сортировать по: <br/><a style="color:#fff" href="#">Алфавиту <img src="down.jpg"/></a></div>
+</div>
+<div style="clear:both"></div>
+</div>
+
+<div style="clear:both"></div>
+<div style="font-size: 12px;
+    margin: 10px 0 0 0;"><span style="margin-right: 20px;">Компания</span> Макс. переходов Объявления  Показы Переходы CTR Действия ? Бюджет Расход Ср. цена </div>
+<div style="font-size: 12px;color: #000;">
+<span style="
+    margin-left: 74px;
+">Будни/выходные</span>
+<span style="
+    margin-left: 22px;
+">активно/всего</span>
+<span style="
+    margin-left: 155px;
+">%конверсий</span>
+<span style="
+    margin-left: 11px;
+">в сутки</span>
+</div>
+
+<div>
+<div style="background:#97bfd9;color:#000;    ">
+
+	<div style="float:left;font-size: 20px;
+    padding: 10px;
+    font-weight: bold;">Итого за весь период</div>
+	
+	<div style="float:left;    margin-left: 30px;margin-top: 14px;">все объявления</div>
+	<div style="clear:both"></div>
+	
+	</div>
+<div style="background:#e9f6ff;color:#000;font-size: 20px;
+    font-weight: bold;    padding: 8px;">
+	<div><img src="com.jpg"/>Название компании</div>
+	<div style="font-size: 10px;margin-top: 10px;"><a style="color:#000" href="#">ограничения</a> <a style="color:#000" href="#">настройки</a> <a style="color:#000" href="#">в архив</a> <a style="color:#000" href="#">удалить</a> <a style="margin-left: 70px;color:#000" href="#">на паузу</a></div>
+	</div>
+</div>
+</div>
+
+
+</div>
+
+<?php }else{ ?>
+
+<style>
+body{background:#1aa0f7;color:#fff;margin:0;padding:0;}
+.active{border:4px #000 solid;padding: 8px 5px 10px 5px;}
+.wbutton{color: #000;
+    background: #fff;
+    border-radius: 15px;
+    padding: 5px;
+    font-size: 11px;text-decoration: none;}
+</style>
+
+
+
+<div style="background:#fff;height:50px;color:#000;margin-top: 20px;">
+
+<a href="#"><img src="login.jpg" style="float: right;padding:10px 15px 10px 5px;"/></a>
+<a href="#"><img src="sett.jpg" style="float: right;padding: 10px 5px;"/></a>
+
+<div style="float:right;font-size:25px;padding:10px;"><a style="text-decoration: none;color: #000;" href="#">Авторынок Лениногорск</a></div>
+<a href="#"><img src="lg.jpg" style="float: right;width:50px;padding: 5px 0 0 0;"/></a>
+
+
+</div>
+
+<div style="background:#3377a4;height:30px;padding:10px 0 0px 0;color:#fff;">
+<a class="active" href="#" style="font-size: 13px;color: #fff;text-decoration: none;margin:  10px 10px 10px 0;">Покупка трафика</a> <a href="#" style="font-size: 13px;color: #fff;text-decoration: none;margin: 10px;">Продажа трафика</a> <a href="#" style="font-size: 13px;color: #fff;text-decoration: none;margin: 10px;">Агентам</a>
+
+<div style="  padding: 5px 10px;  float: right;
+"><a style="" href="#" class="wbutton">Пополнить счет</a></div>
+<div style="   padding: 5px 10px; 
+ float: right;
+    "><a style="" href="#" class="wbutton">История</a> </div>
+<div style="margin-right: 10px;float:right;color:#fff">
+<p style="padding: 0px;
+    margin: 0px;
+    font-size: 12px;">На счете:</p> 
+<p style="padding: 0px;
+    margin: 0px;
+    font-size: 12px;">0.00 р</p>
+ </div>
+
+<div style="font-size: 30px;margin: 40px 0 0 20px;font-weight: bold;">
+Рекламные компании
+</div>
+<div style="clear:both"></div>
+<div style="margin-bottom: 20px;">
+<div style="float:left;margin-right: 10px;">
+<div style="margin: 10px 0 0 35px;
+    font-size: 20px;
+    font-weight: bold;">Ограничение аккаунта</div>
+<div style="clear:both"></div>
+<div style="background:#94caed;float:left;    margin: 15px 5px 0 35px;
+    padding: 10px;width:70px;    height: 200px;
+">
+<div style="font-weight: bold;font-size: 13px;">
+
+<div>Переходы <img width="12" style="float:right" src="down1.jpg"/>
+</div>
+</div>
+
+<div style="font-size: 12px;
+    margin-top: 12px;">
+<div style="margin-bottom: 10px;">Максимум аккаунта <br/><span style="color:#000">не ограничен</span></div>
+<div style="margin-bottom: 10px;">Получено: <span style="color:#000">0</span></div>
+<div style="margin-bottom: 10px;">Суточный лимит</div>
+<div>Будни: <span style="color:#000">макс.</span></div>
+<div>Выходные: <span style="color:#000">макс.</span></div>
+</div>
+
+</div>
+<div style="background:#94caed;float:left;margin: 15px 5px 0 0px;
+    padding: 10px;width:70px;    height: 200px;">
+	<div style="font-weight: bold;font-size: 13px;">Бюджет</div>
+	<div style="font-size: 12px;
+    margin-top: 12px;">
+	<div style="margin-bottom: 10px;">Общий на аккаунт: <br/><span style="color:#000">не ограничен</span></div>
+	<div style="margin-bottom: 10px;">Потрачено: <span style="color:#000">0</span></div>
+	<div style="margin-bottom: 10px;">Суточный лимит</div>
+	<div>Будни: <br/><span style="color:#000">не ограничен</span></div>
+	<div>Выходные: <br/><span style="color:#000">не ограничен</span></div>	
+	</div>
+	</div>
+<div style="background:#94caed;float:left;margin: 15px 5px 0 0px;
+    padding: 10px;width:70px;    height: 200px;">
+	<div style="font-weight: bold;font-size: 13px;">Расписание</div>
+	<div style="font-size: 12px;
+    margin-top: 12px;">
+	<div style="margin-bottom: 10px;">Период трансляции: <br/><span style="color:#000">не ограничен</span></div>
+	<div style="margin-bottom: 10px;">Время: <br/><span style="color:#000">не ограничен</span></div>
+	</div>
+	
+	</div>
+</div>
+<div style="float:left">
+<div  style="font-size: 20px;
+    font-weight: bold;margin-top:10px">Настройки по умолчанию</div>
+<div style="clear:both"></div>
+<div style="background:#94caed;margin: 15px 5px 0 0px;
+    padding: 10px;    height: 200px;
+">
+<div style="font-weight: bold;font-size: 13px;">
+Таргетинг</div>
+	<div style="font-size: 12px;
+    margin-top: 12px;">
+	<div>Регион: <span style="color:#000">не ограничен</span></div>
+<div>Тематика объявления: <span style="color:#000">легковые автомобили</span></div>
+</div>
+</div>
+</div>
+<div style="clear:both"></div>
+</div>
+<div style="clear:both"></div>
+
+
+<div style="margin:0 20px">
+
+<div>
+<div style="float:left">
+<a style="" href="#" class="wbutton">Создать компанию</a> или <a style="" href="#" class="wbutton">Добавить объявление</a>
+</div>
+<div style="float:right;font-size:12px">
+<div style="float:right;">Статистика за период: <br/><a style="color:#fff" href="#"><img src="calendar.jpg"/> Сегодня<img src="down.jpg"/></a></div>
+<div style="float:right;margin-right: 15px;">Сортировать по: <br/><a style="color:#fff" href="#">Алфавиту <img src="down.jpg"/></a></div>
+</div>
+</div>
+
+<div style="clear:both"></div>
+<div style="font-size: 12px;
+    margin: 10px 0 0 0;"><span style="margin-right: 20px;">Компания</span> Макс. переходов Объявления  Показы Переходы CTR Действия ? Бюджет Расход Ср. цена </div>
+<div style="font-size: 12px;color: #000;">
+<span style="
+    margin-left: 74px;
+">Будни/выходные</span>
+<span style="
+    margin-left: 22px;
+">активно/всего</span>
+<span style="
+    margin-left: 155px;
+">%конверсий</span>
+<span style="
+    margin-left: 11px;
+">в сутки</span>
+</div>
+
+<div>
+<div style="background:#97bfd9;color:#000;    ">
+
+	<div style="float:left;font-size: 20px;
+    padding: 10px;
+    font-weight: bold;">Итого за весь период</div>
+	
+	<div style="float:left;    margin-left: 30px;margin-top: 14px;">все объявления</div>
+	<div style="clear:both"></div>
+	
+	</div>
+<div style="background:#e9f6ff;color:#000;font-size: 20px;
+    font-weight: bold;    padding: 8px;">
+	<div><img src="com.jpg"/>Название компании</div>
+	<div style="font-size: 10px;margin-top: 10px;"><a style="color:#000" href="#">ограничения</a> <a style="color:#000" href="#">настройки</a> <a style="color:#000" href="#">в архив</a> <a style="color:#000" href="#">удалить</a> <a style="margin-left: 70px;color:#000" href="#">на паузу</a></div>
+	</div>
+</div>
+</div>
+
+
+</div>
+<?php } ?>
